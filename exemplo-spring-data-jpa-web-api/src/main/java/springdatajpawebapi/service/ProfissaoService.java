0a8aba10096d64065856278441d83e4806b50e41
Gleyson Sampaio <glysns@hotmail.com>
2022-12-16 20:44:53 -0300

    spring data jpa projecao e paginacao

--- exemplo-spring-data-jpa-web-api/src/main/java/springdatajpawebapi/service/ProfissaoService.java
+++ exemplo-spring-data-jpa-web-api/src/main/java/springdatajpawebapi/service/ProfissaoService.java
@@ -1,7 +1,11 @@
 package springdatajpawebapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import springdatajpawebapi.dto.response.ProfissaoResponse;
 import springdatajpawebapi.model.Profissao;
 import springdatajpawebapi.repository.ProfissaoRepository;
 
@@ -19,5 +23,14 @@ public class ProfissaoService {
         //existem outras maneiras de implementar isso
         entity.setNome(request.getNome());
         repository.save(entity);
+    }
+    public Page<ProfissaoResponse> listar(String nome, int page, int size){
+        Pageable paginacao = PageRequest.of(page, size);
+        Page<ProfissaoResponse> result=null;
+        if(nome!=null && (!nome.isEmpty()))
+            result = repository.findByNomeContaining(nome,paginacao);
+        else
+            result =repository.findBy(paginacao);
+        return result;
     }
 }
--- exemplo-spring-data-jpa-web-api/src/main/java/springdatajpawebapi/webservice/ProfissaoResource.java
+++ exemplo-spring-data-jpa-web-api/src/main/java/springdatajpawebapi/webservice/ProfissaoResource.java
@@ -1,8 +1,12 @@
 package springdatajpawebapi.webservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springdatajpawebapi.dto.response.ClienteResponse;
+import springdatajpawebapi.dto.response.ProfissaoResponse;
 import springdatajpawebapi.model.Cliente;
 import springdatajpawebapi.model.Profissao;
 import springdatajpawebapi.repository.ClienteRepository;
@@ -10,7 +14,9 @@ import springdatajpawebapi.repository.ProfissaoRepository;
 import springdatajpawebapi.service.ClienteService;
 import springdatajpawebapi.service.ProfissaoService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/profissoes")
@@ -32,9 +38,24 @@ public class ProfissaoResource {
     public void put(@PathVariable("id") Integer id, @RequestBody Profissao request){//aqui deveria ter o seu DTO
         service.update(id,request);
     }
-    @GetMapping
+    @GetMapping("/all")
     public List<Profissao> getAll(){
         return repository.findAll();
     }
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> search(@RequestParam(required = false) String nome,
+                                                      @RequestParam(defaultValue = "0") int page,
+                                                      @RequestParam(defaultValue = "3") int size){
 
+
+        Page<ProfissaoResponse> result = service.listar(nome,page,size);
+        //este map->responde poderia ser um objeto bem arrumadinho
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", result.getContent());
+        response.put("currentPage", result.getNumber());
+        response.put("totalItems", result.getTotalElements());
+        response.put("totalPages", result.getTotalPages());
+
+       return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
